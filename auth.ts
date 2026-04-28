0c187173793df4545fadc70acc5cbb0ba7c5479f
fatima.ali.abed99 <fatima.ali.abed@gmail.com>
2026-04-28 18:32:14 +0300

    update

---
+++ auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth} from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma'
+
+import prisma from "./repositories/Prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma,{ provider:'postgresql' }),
+  trustedOrigins: ['http://localhost:5173'],
+  emailAndPassword: {
+    enabled: true,
+  },
+});
+
+export type AuthType = {
+  user: typeof auth.$Infer.Session.user | null
+  session: typeof auth.$Infer.Session.session | null
+}
+  
